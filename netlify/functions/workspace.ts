9b15b0eab96682bace071f962a2eb1d604847d9d
SeungHun6450 <toffg6450@naver.com>
2022-07-11 20:20:13 +0900

    edit

--- netlify/functions/workspace.ts
+++ netlify/functions/workspace.ts
@@ -1,14 +1,17 @@
 import { Handler } from '@netlify/functions'
+import axios from 'axios'
 
 const handler: Handler = async (event, context) => {
-await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/notion/workspaces/${id}`, {
+  const { id, method, data } = JSON.parse(event.body as string) as { id: string, method: string, data: unknown }
+  await axios({
+  url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/notion/workspaces/${id}`,
   method,
   headers: {
     'content-type': 'application/json',
     'apikey': 'FcKdtJs202204',
     'username': 'ParkYoungWoong'
   },
-  body: JSON.stringify(body)
+  data
   })
 }
 export { handler }
